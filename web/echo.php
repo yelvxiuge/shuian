aa52a30579556a720a373fff8777a248ababdd8e
sukhoi <sukhoi27i@gmail.com>
2016-09-03 16:38:05 +0800

    auth echostr

---
+++ web/echo.php
@@ -0,0 +1,23 @@
+<?php
+
+function checkSignature()
+{
+    $signature = $_GET["signature"];
+    $timestamp = $_GET["timestamp"];
+    $nonce = $_GET["nonce"];
+
+//    $token = TOKEN;
+//    $tmpArr = array($token, $timestamp, $nonce);
+//    sort($tmpArr, SORT_STRING);
+//    $tmpStr = implode( $tmpArr );
+//    $tmpStr = sha1( $tmpStr );
+//
+//    if( $tmpStr == $signature ){
+//        return true;
+//    }else{
+//        return false;
+//    }
+    echo $_GET['echostr'];
+
+
+}
